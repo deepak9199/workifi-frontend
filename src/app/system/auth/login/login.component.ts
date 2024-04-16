490b69cd0daf02ca256a0818928b47afe67977dc
DEEPK SHARMA <deepakkrsharma5233@gmail.com.com>
2024-04-16 14:21:49 +0530

    login done

--- src/app/system/auth/login/login.component.ts
+++ src/app/system/auth/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '../../../shared/_service/auth.service';
+import { SharedService } from '../../../shared/_service/shared.service';
+import { TokenStorageService } from '../../../shared/_service/token-storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +11,67 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loading: boolean = false
 
+  constructor(
+    private authService: AuthService, private router: Router,
+    private toster: ToastrService,
+    private tokenstorage: TokenStorageService,
+    private sharedService: SharedService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  // login function
+  signIn(email: string, password: string) {
+    if (email === "" && password === "") {
+      this.toster.error("Email && Password is empty")
+    }
+    else if (email === "") {
+      this.toster.error("Email is empty")
+    }
+    else if (password === "") {
+      this.toster.error("Password is empty")
+    }
+    else {
+      // console.log(email, password)
+      this.loginauthapi(email, password)
+    }
+
+  }
+  // login api
+  private loginauthapi(email: string, password: string) {
+    this.loading = true
+    this.authService.signInWithEmailAndPassword(email, password).subscribe(
+      response => {
+        if (response) {
+          // console.log('User Credential:', response.userCredential);
+          // console.log('User token:', response.token);
+          // console.log('User ID:', response.uid);
+          // Redirect to dashboard or perform other actions
+          this.loading = false
+          this.router.navigate(['/']).then(() => {
+            this.toster.success("Login SuccessFully")
+            this.tokenstorage.saveUser((response))
+            this.tokenstorage.saveToken(response.token)
+            this.trigertrefreshnavbar()
+          });
+        } else {
+          // Handle null response, possibly display an error message
+          this.toster.error('Authentication failed')
+          this.loading = false
+        }
+      },
+      error => {
+        console.error('Error signing in:', error);
+        this.toster.error('Error signing in')
+        this.loading = false
+      }
+    );
+  }
+
+  private trigertrefreshnavbar() {
+    this.sharedService.triggerFunction();
+  }
 }
--- src/app/system/auth/logout/logout.component.ts
+++ src/app/system/auth/logout/logout.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '../../../shared/_service/auth.service';
+import { SharedService } from '../../../shared/_service/shared.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logout',
@@ -6,5 +10,37 @@ import { Component } from '@angular/core';
   styleUrl: './logout.component.css'
 })
 export class LogoutComponent {
+  loading: boolean = true
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private toster: ToastrService,
+    private sharedService: SharedService
+  ) { }
 
+  ngOnInit(): void {
+    this.signout()
+  }
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/']).then(() => {
+      this.toster.success('Logout SuccessFully')
+      this.loading = false
+    });
+  }
+  // logout api
+  signout() {
+    this.authService.signOut().subscribe(
+      (response) => {
+        // console.log(response)
+        this.logout()
+      },
+      error => {
+        console.error(error)
+        this.toster.error('error in logout')
+      })
+  }
+  private trigertrefreshnavbar() {
+    this.sharedService.triggerFunction();
+  }
 }
