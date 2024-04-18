6ee50b186e931e750d7b4bece40ce07377e2dc49
DEEPK SHARMA <deepakkrsharma5233@gmail.com.com>
2024-04-18 13:09:02 +0530

    done

--- src/app/shared/default/default.component.ts
+++ src/app/shared/default/default.component.ts
@@ -10,38 +10,5 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class DefaultComponent {
 
-  constructor(
-    private tokenstorage: TokenStorageService,
-    private router: Router,
-    private toster: ToastrService
-  ) { }
-
-  ngOnInit() {
-    if (!this.ValidatorChecker(this.tokenstorage.getToken())) {
-      switch (this.tokenstorage.getUser().role[0]) {
-        case 'client': {
-          this.router.navigate(['/client/dashboard'])
-          break;
-        }
-        case 'freelancer': {
-          this.router.navigate(['/freelancer/projectlist'])
-          break;
-        }
-        default: {
-          this.router.navigate(['/']).then(() => {
-            this.toster.error('Role not Matched')
-          })
-        }
-      }
-    }
-  }
-
-  private ValidatorChecker(data: any) {
-    if (typeof data === "undefined" || data === null || data === '') {
-      return false
-    }
-    else {
-      return true
-    }
-  }
+ 
 }
--- src/app/system/home/home.component.ts
+++ src/app/system/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { TokenStorageService } from '../../shared/_service/token-storage.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,40 @@ import { TokenStorageService } from '../../shared/_service/token-storage.service
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  loading: boolean = false
+  constructor(
+    private tokenstorage: TokenStorageService,
+    private router: Router,
+    private toster: ToastrService
+  ) { }
 
+  ngOnInit() {
+    if (this.ValidatorChecker(this.tokenstorage.getToken())) {
+      switch (this.tokenstorage.getUser().role[0]) {
+        case 'client':
+          // Assuming tokenstorage is accessible and getUser() returns an object with a 'role' array
+          this.router.navigate(['/client/createproject']);
+          break;
 
+        case 'freelancer':
+          this.router.navigate(['/freelancer/projectlist']);
+          break;
+
+        default:
+          this.router.navigate(['/']).then(() => {
+            this.toster.error('Role not Matched'); // Assuming toster is accessible and has an error method
+          });
+
+      }
+
+    }
+  }
+
+  private ValidatorChecker(data: any) {
+    if (typeof data === "undefined" || data === null || data === '') {
+      return false
+    }
+    else {
+      return true
+    }
+  }
 }
