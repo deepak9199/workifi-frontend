f1bb5137e04c0969acee93bdc64459cf41f9d249
DEEPK SHARMA <deepakkrsharma5233@gmail.com.com>
2024-04-29 16:51:53 +0530

    done

--- src/app/shared/_baseurl/firebaseconfig.ts
+++ src/app/shared/_baseurl/firebaseconfig.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyCReQzIBiZi5rr_HDhqkGcHg7TRQ2O_1zw",
-    authDomain: "workifi-55b58.firebaseapp.com",
-    projectId: "workifi-55b58",
-    storageBucket: "workifi-55b58.appspot.com",
-    messagingSenderId: "871858056163",
-    appId: "1:871858056163:web:7926e608b3bd87d1ab0ed6",
-    measurementId: "G-34E3ZX1BD0"
+    apiKey: "AIzaSyAspO_Ho9mwFlSU5zmGqpi696UD9w-kRSQ",
+    authDomain: "workifest-ecd6d.firebaseapp.com",
+    projectId: "workifest-ecd6d",
+    storageBucket: "workifest-ecd6d.appspot.com",
+    messagingSenderId: "104754414980",
+    appId: "1:104754414980:web:0e5fed32412b4ca19d89d1",
+    measurementId: "G-BKKB2W057V"
 };
